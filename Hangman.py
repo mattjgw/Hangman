0c8393c0d3cd6a25fbacc8fb2361b296717aa4f7
Matthew Wright <mattjgw@gmail.com>
2017-10-26 21:10:19 -0400

    The man is now getting hung

--- Hangman.py
+++ Hangman.py
@@ -5,11 +5,15 @@ complete = False
 print("Hello! Welcome to Hangman!\nPlease guess a letter or a word:")
 guess = input()
 while not complete:
-    if word.contains(guess):
-        word.evaluate(guess)
+    word.evaluate(guess)
+    word.drawMan()
     word.print()
     if word.is_complete():
         print("\nCongratulations! You have guessed the word correctly")
         complete = True
     else:
         guess = input("\nGuess a letter or a word\n")
+
+word = Word("apple")
+
+word.drawMan()
--- Word.py
+++ Word.py
@@ -2,11 +2,13 @@ class Word:
 
     secret_word = ""
     which_letters_guessed = []
+    strikes = 0
 
     def __init__(self, chosen_word="apple"):
         self.secret_word = chosen_word
         for _ in self.secret_word:
             self.which_letters_guessed.append(False)
+        self.strikes = 0
 
     def contains(self, guessed_letter):
         if guessed_letter in self.secret_word:
@@ -16,11 +18,15 @@ class Word:
 
     def evaluate(self, guess):
         if len(guess) is 1:
-            i = 0
-            for letter in self.secret_word:
-                if letter == guess:
-                    self.which_letters_guessed[i] = True
-                i += 1
+            if self.secret_word.__contains__(guess):
+                i = 0
+                for letter in self.secret_word:
+                    if letter == guess:
+                        self.which_letters_guessed[i] = True
+                    i += 1
+            else:
+                self.strikes += 1
+
         else:
             if guess == self.secret_word:
                 i = 0
@@ -42,6 +48,27 @@ class Word:
             if letter is False:
                 return False
         return True
+
+    def drawMan(self):
+        self.strikes = 10
+        if self.strikes is 0:
+            return
+        elif self.strikes >= 1:
+            print("_____________")
+            print("    |       |")
+            print("( ͡° ͜ʖ ͡°)    |")
+            print("    ^       |")
+            if self.strikes >= 2:
+                print("    |       |")
+                print("    |       |")
+                if self.strikes >= 3:
+                    print(" ___|___    |")
+                    print("    |       |")
+                    if self.strikes >= 4:
+                        print("   /  \\     |")
+                        print("  /    \\    |")
+
+
 
 
 
