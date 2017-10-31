80533bd75c3e14d91883f65ac34c9fbce2bc0237
Matthew Wright <mattjgw@gmail.com>
2017-10-31 18:34:56 -0400

    Code cleanup and commenting

--- Hangman.py
+++ Hangman.py
@@ -5,22 +5,23 @@ words = ["apple", "peach", "banana", "pumpkin", "strawberry", "melon", "orange"]
 play = True
 
 while play:
-    word = Word(words[random.randrange(0, 7)])
+    word = Word(words[random.randrange(0, 7)])  # Create a word object using a random word from the words list
     complete = False
     print("Hello! Welcome to Hangman!\nPlease guess a letter or a word:")
     word.print()
     guess = input()
     while not complete:
         word.evaluate(guess)
-        word.drawMan()
+        word.draw_man()
         word.print()
         if word.is_complete():
             complete = True
             answer = input("Would you like to play again? (Answer yes or no)")
             if answer.lower() == "yes":
                 continue
-            else:
+            else:  # If user answers anything but yes, do not play again
                 play = False
 
         else:
             guess = input("\nGuess a letter or a word\n")
+print("Goodbye!")
--- Word.py
+++ Word.py
@@ -6,18 +6,12 @@ class Word:
 
     def __init__(self, chosen_word="apple"):
         self.secret_word = chosen_word
-        self.which_letters_guessed = []
-        for i in self.secret_word:
+        self.which_letters_guessed = []  # Ensures the which letters guessed list is empty before appending
+        for _ in self.secret_word:
             self.which_letters_guessed.append(False)
         self.strikes = 0
 
-    def contains(self, guessed_letter):
-        if guessed_letter in self.secret_word:
-            return True
-        else:
-            return False
-
-    def evaluate(self, guess):
+    def evaluate(self, guess):  # Checks to see the result of the guess
         if len(guess) is 1:
             if self.secret_word.__contains__(guess):
                 i = 0
@@ -31,20 +25,22 @@ class Word:
         else:
             if guess == self.secret_word:
                 i = 0
-                for letter in self.which_letters_guessed:
+                for _ in self.which_letters_guessed:
                     self.which_letters_guessed[i] = True
                     i += 1
+            else:
+                self.strikes += 1
 
-    def print(self):
+    def print(self):  # Prints the word with proper letters and blanks
         i = 0
         for letter in self.secret_word:
             if self.which_letters_guessed[i] is True:
-                print(letter, end='')
+                print(letter, end='')  # Print the letters so as to not print with a new line
             else:
-                print('_', end='')
+                print('_', end='')  # Print the blanks so as to not print with a new line
             i += 1
 
-    def is_complete(self):
+    def is_complete(self):  # Checks to see if the game is complete
         if self.strikes >= 4:
             print("\nOh no! You've hung the man. Better luck next time")
             return True
@@ -55,7 +51,7 @@ class Word:
             print("\nCongratulations! You have deduced the word")
             return True
 
-    def drawMan(self):
+    def draw_man(self):  # Draws the hangman based on the strike count
         if self.strikes is 0:
             return
         elif self.strikes >= 1:
